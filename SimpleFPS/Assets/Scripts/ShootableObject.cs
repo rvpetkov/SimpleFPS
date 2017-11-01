6f55ad705d56cda3b39c904f8c4fe5e194b6fe08
rvpetkov <rvpetkov@gmail.com>
2017-11-01 19:09:51 +0200

    Added class

---
+++ SimpleFPS/Assets/Scripts/ShootableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootableObject : MonoBehaviour {
+
+    public int health = 1;
+
+	public void DealDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+}
