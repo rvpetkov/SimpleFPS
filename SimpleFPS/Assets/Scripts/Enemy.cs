e28294d561b815d41afa5cb1838e47827336ea62
rvpetkov <rvpetkov@gmail.com>
2017-11-01 23:54:33 +0200

    Added audio

--- SimpleFPS/Assets/Scripts/Enemy.cs
+++ SimpleFPS/Assets/Scripts/Enemy.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 
+    public GameObject player;
+    public float detectionRadius = 30f;
+
+    private NavMeshAgent agent;
+
 	// Use this for initialization
 	void Start () {
-		
+        agent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if ((agent != null) && (Vector3.Distance(transform.position, player.transform.position) < detectionRadius))
+        {
+            agent.SetDestination(player.transform.position);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
 }
--- SimpleFPS/Assets/Scripts/ShootableObject.cs
+++ SimpleFPS/Assets/Scripts/ShootableObject.cs
@@ -5,13 +5,37 @@ using UnityEngine;
 public class ShootableObject : MonoBehaviour {
 
     public int health = 1;
+    [Tooltip("Whis audio will play when the object is destroyed.")]
+    public AudioSource audioOnDestroy;
 
-	public void DealDamage(int damage)
+    private WaitForSeconds audioDuration;
+    private bool isDead = false;
+
+    void Start()
+    {
+        if (audioOnDestroy)
+            audioDuration = new WaitForSeconds(audioOnDestroy.clip.length);
+    }
+
+    public void DealDamage(int damage)
     {
         health -= damage;
         Debug.Log(health);
 
-        if (health <= 0)
-            Destroy(gameObject);
+        if (health <= 0 && !isDead)
+        {
+            isDead = true;
+            StartCoroutine(DieEffect());
+        }
+    }
+
+    private IEnumerator DieEffect()
+    {
+        if (audioOnDestroy != null)
+            audioOnDestroy.Play();
+
+        yield return audioDuration;
+
+        Destroy(gameObject);
     }
 }
