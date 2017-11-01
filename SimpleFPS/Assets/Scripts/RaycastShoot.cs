6a2119d065d6f53415cc4923df83a08b89362ae0
rvpetkov <rvpetkov@gmail.com>
2017-11-01 18:30:12 +0200

    Class Added - RaycastShoot: Shooting with Raycasts

---
+++ SimpleFPS/Assets/Scripts/RaycastShoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour {
+
+    #region Public members
+
+    public float weaponDamage;
+    public float weaponRange;
+    public float fireRate;
+    public float hitForce;
+    public Transform weaponEnd;
+
+    #endregion
+
+    #region Private members
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
